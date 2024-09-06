83c1e14a68aed5e120fcfc14db1d317cc073324e
Neg00se <evg.matyushko@gmail.com>
2024-09-06 14:20:28 +0300

    Added task repository

---
+++ DataAccessLayer/Interfaces/ITaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using UserTask = DataAccessLayer.Entities.Task;
+
+namespace DataAccessLayer.Interfaces;
+public interface ITaskRepository
+{
+    Task<List<UserTask>> GetAllTasksAsync();
+
+    Task<UserTask> GetByIdAsync(Guid id);
+
+    Task CreateTaskAsync(UserTask task);
+
+    void Update(UserTask task);
+
+    void Delete(Guid id);
+}
---
+++ DataAccessLayer/Repositories/TaskRepository.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using UserTask = DataAccessLayer.Entities.Task;
+
+namespace DataAccessLayer.Repositories;
+public class TaskRepository : ITaskRepository
+{
+    private readonly TaskSystemDbContext _context;
+
+    public TaskRepository(TaskSystemDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateTaskAsync(UserTask task)
+    {
+        if (task is not null)
+        {
+            await _context.Tasks.AddAsync(task);
+
+        }
+    }
+
+    public void Delete(Guid id)
+    {
+        var task = _context.Tasks.Find(id);
+        if (task is not null)
+        {
+            _context.Tasks.Remove(task);
+        }
+    }
+
+    public async Task<List<UserTask>> GetAllTasksAsync()
+    {
+        var tasks = await _context.Tasks.ToListAsync();
+        return tasks;
+    }
+
+    public async Task<UserTask> GetByIdAsync(Guid id)
+    {
+        var task = await _context.Tasks.FindAsync(id);
+        return task;
+    }
+
+    public void Update(UserTask task)
+    {
+        _context.Tasks.Update(task);
+    }
+}
