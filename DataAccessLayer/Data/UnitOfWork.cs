87b8bb08908f7f6b328057f2050eb801a44b7df6
Neg00se <evg.matyushko@gmail.com>
2024-09-06 14:19:54 +0300

    Added unit of work

---
+++ DataAccessLayer/Data/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Interfaces;
+using DataAccessLayer.Repositories;
+
+namespace DataAccessLayer.Data;
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly TaskSystemDbContext _context;
+
+    public UnitOfWork(TaskSystemDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public IUserRepository UserRepository => new UserRepository(_context);
+
+    public ITaskRepository TaskRepository => new TaskRepository(_context);
+
+    public async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+}
---
+++ DataAccessLayer/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccessLayer.Interfaces;
+public interface IUnitOfWork
+{
+    ITaskRepository TaskRepository { get; }
+    IUserRepository UserRepository { get; }
+
+    Task SaveAsync();
+}
