2057b52dd42d38a1adc04bc8027759a4c5988361
Neg00se <evg.matyushko@gmail.com>
2024-09-07 21:00:27 +0300

    Added registration password validation

---
+++ ServiceLayer/Validation/PasswordValidation.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ServiceLayer.Validation;
+public static class PasswordValidation
+{
+
+
+    public static void ValidateRegistrationPassword(string password)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+
+            throw new ArgumentNullException(nameof(password));
+        }
+
+        if (password.Length < 5)
+        {
+            throw new Exception();
+        }
+        if (!Regex.IsMatch(password, @"[a-z]"))
+        {
+            throw new Exception();
+        }
+        if (!Regex.IsMatch(password, @"[A-Z]"))
+        {
+            throw new Exception();
+        }
+        if (!Regex.IsMatch(password, @"\d"))
+        {
+            throw new Exception();
+        }
+        if (!Regex.IsMatch(password, @"[^\w]"))
+        {
+            throw new Exception();
+        }
+    }
+
+
+
+
+
+}
+
