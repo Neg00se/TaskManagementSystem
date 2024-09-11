a8255274c5e3520868aba407f00a2fbc084a2324
Neg00se <evg.matyushko@gmail.com>
2024-09-11 22:13:51 +0300

    Filter model Replaced with Query model

--- ServiceLayer/Models/FilterModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using DataAccessLayer.Enums;
-
-namespace ServiceLayer.Models;
-public class FilterModel
-{
-    public DateTime? DueDate { get; set; }
-    public PriorityEnum? Priority { get; set; }
-    public StatusEnum? Status { get; set; }
-}
---
+++ ServiceLayer/Models/QueryModel.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Enums;
+
+namespace ServiceLayer.Models;
+public class QueryModel
+{
+    public DateTime? DueDate { get; set; } = null;
+
+    public PriorityEnum? Priority { get; set; } = null;
+
+    public StatusEnum? Status { get; set; } = null;
+
+    public string? SortBy { get; set; } = null;
+
+    public bool IsDescending { get; set; } = false;
+
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize { get; set; } = 20;
+}
