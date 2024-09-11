f25583dfe555aec3ab12f886affee9a20b802f93
Neg00se <evg.matyushko@gmail.com>
2024-09-11 22:15:45 +0300

    Added interface for task service

---
+++ ServiceLayer/Interfaces/ITaskService.cs
@@ -0,0 +1,11 @@
+﻿using ServiceLayer.Models;
+
+namespace ServiceLayer.Interfaces;
+public interface ITaskService
+{
+    Task AddTaskAsync(TaskModel model, string userId);
+    Task DeleteUserTask(string taskId);
+    Task<DataAccessLayer.Entities.Task> GetUserTaskByIdAsync(string userId, string taskId);
+    Task<IEnumerable<DataAccessLayer.Entities.Task>> GetUserTasksAsync(string userId, QueryModel query);
+    Task UpdateUserTaskAsync(TaskModel task, string id);
+}
