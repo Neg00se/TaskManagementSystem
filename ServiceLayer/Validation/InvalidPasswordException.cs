3b0853bb215638388b6ba8072cd9cf3f6708e6d1
Neg00se <evg.matyushko@gmail.com>
2024-09-11 22:14:49 +0300

    added esception for invalid password

---
+++ ServiceLayer/Validation/InvalidPasswordException.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiceLayer.Validation;
+public class InvalidPasswordException : Exception
+{
+    public InvalidPasswordException()
+    {
+    }
+
+    public InvalidPasswordException(string? message) : base(message)
+    {
+    }
+
+    public InvalidPasswordException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
