c2e497da7e41e28545cc015b37b374304435f1c4
Neg00se <evg.matyushko@gmail.com>
2024-09-11 22:18:23 +0300

    Added User Controler

---
+++ TaskManagementSystem/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Interfaces;
+using ServiceLayer.Models;
+using ServiceLayer.Validation;
+
+namespace TaskManagementSystem.Controllers;
+[Route("/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+
+    [HttpPost("register")]
+    public async Task<ActionResult> Create(UserRegisterModel model)
+    {
+        try
+        {
+            await _userService.RegisterAsync(model);
+            return Ok();
+        }
+        catch (EmailOrUsernameAlreadyExistException ex)
+        {
+
+            return BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500);
+        }
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult> Get(UserLoginModel model)
+    {
+        try
+        {
+            var token = await _userService.LoginAsync(model);
+            return Ok(token);
+        }
+        catch (IncorrectPasswordException ex)
+        {
+
+            return BadRequest(ex.Message);
+        }
+        catch (ArgumentNullException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500);
+        }
+    }
+}
